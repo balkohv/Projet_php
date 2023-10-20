02d0d175b8969f09357d697fed5c36efb9345797
balkohv <theo.pellefigue@gmail.com>
2023-10-20 09:46:51 +0200

    nav bar

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-    echo "coucou";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>accueil</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Projet</h1>
+        </header>
+        <nav>
+            <ul class="nav-bar">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="index.php?page=contact">Contact</a></li>
+                <li><a href="index.php?page=inscription">Inscription</a></li>
+                <li><a href="index.php?page=connexion">Connexion</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
